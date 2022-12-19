d385845f43a85e0aee76b7c75dd3cb368b7a17b4
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2022-12-19 11:56:25 -0300

    Primeiro Commit

---
+++ BasicaoSobreStrings/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace BasicaoSobreStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            string sobrenome;
+            string nome_completo;
+
+            Console.WriteLine("Digite seu nome");
+            nome = Console.ReadLine();
+
+            Console.WriteLine("Digite seu sobrenome");
+            sobrenome = Console.ReadLine();
+
+            nome_completo = nome + " " + sobrenome;
+            Console.WriteLine("Olá " + nome_completo);
+            Console.WriteLine("Seu nome tem " + nome.Length + " caracteres");
+            Console.WriteLine("Olá " + nome.ToUpper()     );
+            Console.WriteLine("Olá " + nome.ToLower()     );
+            Console.WriteLine("Olá " + nome_completo.ToUpper() );
+
+            string fragmento = nome_completo.Substring(3, 3);
+            Console.WriteLine("Fragmento = {0} " ,fragmento);
+
+            Console.ReadKey(); //pausa;...
+        }
+    }
+}
